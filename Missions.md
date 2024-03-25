ba0a32d66ffa1d385c55e242c069fe8b4ad66768
Ariel <arieldk6@gmail.com>
2024-03-26 00:43:08 +0200

    added worker service in docker

--- Missions.md
+++ Missions.md
@@ -1,4 +1,1 @@
-1. Configure a seperate service for a worker
-2. finish adjusting docker-compose fiel that includes -> 
-    django service, redis service, celery_worker.
-3. after all that consider adding postgres for general DB
+1. after all that consider adding postgres for general DB
--- server/server/celery.py
+++ server/server/celery.py
@@ -1,9 +1,10 @@
 import os
 from celery import Celery
+from decouple import config
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
 
-app = Celery("server")
+app = Celery("server", broker=config("CELERY_BROKER_REDIS_URL"))
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
